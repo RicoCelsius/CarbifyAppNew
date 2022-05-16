19620640503a266cc79e5bc0073eb12c4371d99d
RicoCelsius <rico.aarntzen@gmail.com>
2022-05-16 14:56:04 +0200

    added camera.js askpermissions function

--- Carbify-App/App.js
+++ Carbify-App/App.js
@@ -1,21 +1,15 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ScanScreen  from "./screens/ScanScreen.js"; 
-
+import ScanScreen from "./screens/ScanScreen.js";
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
       <Button
-<<<<<<< HEAD
         title="Go to Scan"
         onPress={() => navigation.navigate("ScanScreen")}
-=======
-        title="Go to Details"
-        onPress={() => navigation.navigate("Home")}
->>>>>>> 9470e767d6407f217200d43d3791804c23df919c
       />
     </View>
   );
@@ -28,16 +22,11 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-<<<<<<< HEAD
         <Stack.Screen name="ScanScreen" component={ScanScreen} />
-=======
-        <Stack.Screen name="Details" component={Home} />
->>>>>>> 9470e767d6407f217200d43d3791804c23df919c
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
--- Carbify-App/screens/ScanScreen.js
+++ Carbify-App/screens/ScanScreen.js
@@ -1,9 +1,88 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput, Image } from "react-native";
+import { useEffect, useState } from "react";
+import { BarCodeScanner } from "expo-barcode-scanner";
+//import Camera from "./screens/Camera.js";
 
 export default function ScanScreen() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("Not yet scanned");
+
+  useEffect(() => {
+    askPermissions();
+  }, []);
+
+  const askPermissions = () => {
+    (async () => {
+      console.log("Asking for permissions");
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status == "granted");
+      openCamera();
+    })();
+  };
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  function openCamera() {
+    if (hasPermission === true) {
+      console.log("Camera opened, permission true");
+      return (
+        <View>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={{ height: 400, width: 400 }}
+          />
+        </View>
+      );
+    }
   }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          padding: 100,
+        }}
+      >
+        <Image
+          source={{
+            uri: "https://i.ibb.co/tmvt00w/logo.png",
+          }}
+          style={{ height: 300, width: 300, borderRadius: 1000 }}
+        />
+
+        <TextInput
+          placeholder="Enter barcode"
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+            padding: 10,
+            marginBottom: 10,
+          }}
+        />
+        <Button title="Open camera" onPress={() => askPermissions()} />
+        <StatusBar style="auto" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
