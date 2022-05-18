b0b8fcd4d3a96578f436ed0e2c73dd057c4adcc5
Kurtsuside <493105@student.fontys.nl>
2022-05-18 11:47:26 +0200

    dope

---
+++ API/Rest/dbconfig.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//     host     : 'carbifyfontys.mysql.database.azure.com',
+//     user     : 'EnzoKnol',
+//     password : 'KnolPower!',
+//     database : 'users',
+//     ssl: true
+// });
+
+const connection=mysql.createConnection({host:"carbifyfontys.mysql.database.azure.com", user:"EnzoKnol", password:"KnolPower!", database:"users", port:3306, ssl:{ca:fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}});
+
+connection.connect((error) => {
+    if(error){
+        console.log(error.message);
+    }
+});
+
+module.exports = connection;
